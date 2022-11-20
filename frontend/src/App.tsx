a370fffc592466cccfad355bee8e234295736a27
Thomas Paulo Jr <thomaspaulo@hotmail.com>
2022-11-20 18:26:04 -0300

    Other Components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,19 @@
-import NotificationButton from "./componets/NotificationButton"
+import Header from "./componets/Header"
+import SalesCard from "./componets/SalesCard"
 
 function App() {
- return(
+  return (
   <>
-  <h1>Camila gostosona!!</h1>
-  <NotificationButton />
-  <NotificationButton />
-  </>
- )
+      <Header />
+      <main>
+        <section id="sales">
+          <div className="dsmeta-container"> 
+            <SalesCard />
+            </div>
+        </section>
+      </main>
+      </>
+      )
 }
 
-export default App
+      export default App
