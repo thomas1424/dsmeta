8ff5ca15eed9d4a975bcde3164d0ebdb7773f492
Thomas Paulo Jr <thomaspaulo@hotmail.com>
2022-11-20 17:22:00 -0300

    First component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,12 @@
+import NotificationButton from "./componets/NotificationButton"
 
 function App() {
  return(
+  <>
   <h1>Camila gostosona!!</h1>
+  <NotificationButton />
+  <NotificationButton />
+  </>
  )
 }
 
